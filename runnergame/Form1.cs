677d535a65b698226efbe1dafc2e4e5f57ac288b
chughtai77 <60922177+chughtai77@users.noreply.github.com>
2022-12-28 14:00:06 +0500

    Setting the position of cars (Obstacles) out  of the frame so when game start it will move farwad to the charachter

--- runnergame/Form1.cs
+++ runnergame/Form1.cs
@@ -56,9 +56,34 @@ namespace runnergame
 
         }
 
-        private void GamePlayAgain() { 
-        
+        private void GamePlayAgain() {
 
+            force = 12;
+            jumpspeed = 0; 
+            jump= false;
+            score = 0;
+            obstaclespeed= 10;
+            txtScore.Text = "Score: "+ score;
+            trex.Image = Properties.Resources.sonic2c;
+            isGameOver= false;
+            trex.Top = 304;
+
+
+
+        //Setting the position of cars (Obstacles) out
+        //    of the frame so when game start it will move farwad to the charachter
+            foreach (Control x in this.Controls) {
+
+                if (x is PictureBox && (string)x.Tag == "obstacle") {
+
+                    currentposition = this.ClientSize.Width + rand.Next(10, 10) + (x.Width * 1);
+
+                    x.Left = currentposition; 
+                
+                }
+            }
+
+           
 
         }
     }
